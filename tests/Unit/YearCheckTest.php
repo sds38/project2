76cc484d9f16d416420ab0099cc26036aa4beae8
shail <sds38@njit.edu>
2019-08-03 23:25:47 -0400

    year check test

---
+++ tests/Unit/YearCheckTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tests\Unit;
+use App\Cars;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class CheckCarYearTypeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCarYearType()
+    {
+
+        factory(Cars::class)->create();
+        $car =  Cars::first();
+        $this->assertTrue(is_int($car->year));
+    }
+}
