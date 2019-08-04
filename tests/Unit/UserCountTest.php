f7c1e1cf5f35ed8988462e8afbdcc18cc97754ed
shail <sds38@njit.edu>
2019-08-03 23:02:37 -0400

    User Count Test

---
+++ tests/Unit/UserCountTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tests\Unit;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class CountUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountUsersIs50()
+    {
+        $userCount = 50;
+        $users = factory(User::class, 50 )->create();
+        $this->assertLessThanOrEqual( $userCount, \count( $users ) );
+        $this->assertTrue(true);
+    }
+}
