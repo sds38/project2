764a8961431f4b0b84ce267279f674430fdca5ac
shail <sds38@njit.edu>
2019-08-03 23:23:27 -0400

    count cars test

--- tests/Unit/CarDeleteTest.php
+++ tests/Unit/CarDeleteTest.php
@@ -4,23 +4,21 @@ use App\Cars;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-class CarYearUpdateTest extends TestCase
+class CarDeleteTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testCarYearUpdate()
+    public function testCarDelete()
     {
-        $data['year'] = '2000';
 
         factory(Cars::class)->create();
 
-        $user = Cars::first();
-        $user->update($data);
-        $this->assertInstanceOf(Cars::class, $user);
-        $this->assertEquals($data['year'], $user->year);
+        $car = Cars::first();
+        if($car)
+            $car->delete();
         $this->assertTrue(true);
     }
 }
--- tests/Unit/InsertCarTest.php
+++ tests/Unit/InsertCarTest.php
@@ -1,27 +1,26 @@
-
 <?php
 namespace Tests\Unit;
-use App\Car;
+use App\Cars;
 use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-class CarInsertTest extends TestCase
+class InsertCarTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testCarInsert()
+    public function testInsertCar()
     {
         $data =  [
-            'make' => 'honda',
+            'make' => 'ford',
             'model' => 'Test Model',
             'year' => Carbon::now()->year,
         ];
-        $car = Car::create($data);
-        $this->assertInstanceOf(Car::class, $car);
+        $car = Cars::create($data);
+        $this->assertInstanceOf(Cars::class, $car);
         $this->assertEquals($data['make'], $car->make);
         $this->assertEquals($data['model'], $car->model);
         $this->assertEquals($data['year'], $car->year);
