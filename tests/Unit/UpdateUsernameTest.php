fa8f27ac02a961bddd240e8e2635375d851bb71c
shail <sds38@njit.edu>
2019-08-03 23:00:08 -0400

    update username test

---
+++ tests/Unit/UpdateUsernameTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Unit;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class UpdateUserNameTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUser()
+    {
+        //creates user if doesnt exist
+        factory(User::class)->create();
+        $data['name'] = 'Steve Smith';
+        $user = User::first();
+        $user->update($data);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($data['name'], $user->name);
+        $this->assertTrue(true);
+    }
+}
