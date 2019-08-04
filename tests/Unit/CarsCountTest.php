c3edfd650584c46ab7763b0289534e9d7bca922d
shail <sds38@njit.edu>
2019-08-03 23:25:29 -0400

    count cars test

---
+++ tests/Unit/CarsCountTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit;
+use App\Cars;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class CountCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountCarTest()
+    {
+        $carCount = 50;
+
+        $cars = factory(Car::class, $carCount )->create();
+        $this->assertLessThanOrEqual( $carCount, \count( $cars ) );
+        $this->assertTrue(true);
+    }
+}
